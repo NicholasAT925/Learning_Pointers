944fe87047ae6cb4ef00224ee3bbfee9ee25bdc6
Zuriel <2001555@sit.singaporetech.edu.sg>
2022-05-05 17:27:10 +0800

    Explored the basic ways to references the pointer and all is basic uses

--- In_C/main.c
+++ In_C/main.c
@@ -7,7 +7,22 @@ int getNumber();
 
 /* Global Variables */
 int number; // int variables
-int *pnumber; // pointer int variables
+int *pnumber = NULL; // Declaring pointer int variables
+
 int main(){
+    number = 10;
+    pnumber = &number;
+
+    printf("The variable 'number' has the value of = %d\n", number);
+    printf("The address of number is = %p\n", &number);
+
+    printf("The pointer pnumber has the value of = %p\n", pnumber);
+    printf("The pointer pnumber dereferences the value = %d\n", *pnumber);
+    printf("The pointer itself has the address of = %p\n",&pnumber);
+    printf("The pointer size is = %zd bytes\n", sizeof(pnumber));
     return 0;
+}
+
+int getNumber(){
+
 }
