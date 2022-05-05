4645ae1adc10940121bfb204dbacdcdaafe79b08
Zuriel <2001555@sit.singaporetech.edu.sg>
2022-05-05 16:08:08 +0800

    Initial commit

---
+++ In_C/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/* Function Declerations */
+int getNumber();
+
+/* Global Variables */
+int number; // int variables
+int *pnumber; // pointer int variables
+int main(){
+    return 0;
+}
