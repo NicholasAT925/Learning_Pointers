4dd92fcc35b238dbe2435d4e16370cff43b8c1ca
Zuriel <2001555@sit.singaporetech.edu.sg>
2022-05-05 18:25:45 +0800

    comments

--- In_C/main.c
+++ In_C/main.c
@@ -13,9 +13,10 @@ int main(){
     number = 10;
     pnumber = &number;
 
+    /* Using the int variable itself */
     printf("The variable 'number' has the value of = %d\n", number);
     printf("The address of number is = %p\n", &number);
-
+    /* Referencing and using the pointer */ 
     printf("The pointer pnumber has the value of = %p\n", pnumber);
     printf("The pointer pnumber dereferences the value = %d\n", *pnumber);
     printf("The pointer itself has the address of = %p\n",&pnumber);
