d9782a8bf3915027d2816b9e302856acc1b4db66
Zuriel <2001555@sit.singaporetech.edu.sg>
2022-05-06 22:13:14 +0800

    Update main.c

--- In_C/main.c
+++ In_C/main.c
@@ -19,18 +19,22 @@ int main(){
     /* Using the int variable itself */
     printf("The variable 'number' has the value of = %d\n", number);
     printf("The address of number is = %p\n", &number);
+
     /* Referencing and using the pointer */ 
     printf("The pointer pnumber has the value of = %p\n", pnumber);
     printf("The pointer pnumber dereferences the value = %d\n", *pnumber);
     printf("The pointer itself has the address of = %p\n",&pnumber);
     printf("The pointer size is = %zd bytes\n\n", sizeof(pnumber));
+
     /* Pointer Arithmatics and how arrays and poiter are kinda the same */
     for (i = 0; i < strlen(multiple); ++i){
         printf("multiple[%d] = %c | *(p+%d)= %c | &multiple[%d] = %p | p+%d = %p\n", i, multiple[i], i, *(p + i), i, &multiple[i], i, p + i);
     }
+
     /* Benifits of using pointer instead of arrays */
     copyString(String1, String2);
     printf("\n\nThe text in String2 = %s\n", String2);
+    
     /* Pointer Arithematics */
     printf("\n\nThe length of the string'Hello' = %d\n", countStringLen("Hello"));
     printf("The length of the string '' = %d\n", countStringLen(""));
