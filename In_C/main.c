b9eabfc73af60c4728544a939ffbc1c0f0db6406
Zuriel <2001555@sit.singaporetech.edu.sg>
2022-05-06 19:20:02 +0800

    pointer and arrays

--- In_C/main.c
+++ In_C/main.c
@@ -6,8 +6,10 @@
 int getNumber();
 
 /* Global Variables */
-int number; // int variables
+int number, i; // int variables
 int *pnumber = NULL; // Declaring pointer int variables
+char multiple[] = "a string"; // // char string variable
+char *p = multiple; // char pointer
 
 int main(){
     number = 10;
@@ -20,7 +22,11 @@ int main(){
     printf("The pointer pnumber has the value of = %p\n", pnumber);
     printf("The pointer pnumber dereferences the value = %d\n", *pnumber);
     printf("The pointer itself has the address of = %p\n",&pnumber);
-    printf("The pointer size is = %zd bytes\n", sizeof(pnumber));
+    printf("The pointer size is = %zd bytes\n\n", sizeof(pnumber));
+    /* Pointer Arithmatics and how arrays and poiter are kinda the same */
+    for (i = 0; i < strlen(multiple); ++i){
+        printf("multiple[%d] = %c | *(p+%d)= %c | &multiple[%d] = %p | p+%d = %p\n", i, multiple[i], i, *(p + i), i, &multiple[i], i, p + i);
+    }
     return 0;
 }
 
