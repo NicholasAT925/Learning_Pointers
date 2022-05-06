6da5d61bf60f47dd715fbc2b71707ac89173da1a
Zuriel <2001555@sit.singaporetech.edu.sg>
2022-05-06 22:11:30 +0800

    Pointer and array uses together

--- In_C/main.c
+++ In_C/main.c
@@ -4,6 +4,7 @@
 
 /* Function Declerations */
 void copyString(char *from, char *to);
+int countStringLen(const char *String);
 
 /* Global Variables */
 int number, i; // int variables
@@ -30,17 +31,34 @@ int main(){
     /* Benifits of using pointer instead of arrays */
     copyString(String1, String2);
     printf("\n\nThe text in String2 = %s\n", String2);
+    /* Pointer Arithematics */
+    printf("\n\nThe length of the string'Hello' = %d\n", countStringLen("Hello"));
+    printf("The length of the string '' = %d\n", countStringLen(""));
+    printf("The length of the string 'Dragon' = %d\n", countStringLen("Dragon"));
 
     return 0;
 }
 
 /* Function Details */
 void copyString(char *from, char *to){
-/*  This Functions demostaits how little code needs to be writen vs calling
-    up the array through variables normally
-*/
+    /*  
+     *  This Functions demostaits how little code needs to be writen vs calling
+     *  up the array through variables normally
+    */
     while ( *from){ // Will return false when reaches the end of the string as there iS A '\0' AT THE END
         *to++ = *from++; // return the variable before incrementing
     }
     *to = '\0';
+}
+int countStringLen(const char *String){
+    /* 
+    *   This Function will return the length of the String Input
+    *   Another Example how pointers in array make things simplyfied and easier to find and use info
+    */
+    const char *lastAddress = String; // pointer variable that is use to store the last address of the string
+    while (*lastAddress){ // Will return false when reaches the end of the string as there iS A '\0' AT THE END
+        ++lastAddress; // increment the address or pointer to the next letter until '\0'
+    }
+    return lastAddress - String;    // By taking the last address and minusing it witht eh last address we can find the length
+                                    // As String Addresses for each letter ot element are place together in sequence 
 }
