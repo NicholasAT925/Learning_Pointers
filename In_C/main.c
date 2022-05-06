c8e89f3ec34644433ca1752c112893492cd42392
Zuriel <2001555@sit.singaporetech.edu.sg>
2022-05-06 19:53:20 +0800

    Benefits of pointer over arrays

--- In_C/main.c
+++ In_C/main.c
@@ -3,12 +3,12 @@
 #include <stdlib.h>
 
 /* Function Declerations */
-int getNumber();
+void copyString(char *from, char *to);
 
 /* Global Variables */
 int number, i; // int variables
 int *pnumber = NULL; // Declaring pointer int variables
-char multiple[] = "a string"; // // char string variable
+char multiple[] = "a string", String1[] = "A string to be copied", String2[100]; // char string variable
 char *p = multiple; // char pointer
 
 int main(){
@@ -27,9 +27,16 @@ int main(){
     for (i = 0; i < strlen(multiple); ++i){
         printf("multiple[%d] = %c | *(p+%d)= %c | &multiple[%d] = %p | p+%d = %p\n", i, multiple[i], i, *(p + i), i, &multiple[i], i, p + i);
     }
+    /* Benifits of using pointer instead of arrays */
+    copyString(String1, String2);
+    printf("\n\nThe text in String2 = %s\n", String2);
+    
     return 0;
 }
 
-int getNumber(){
-
+void copyString(char *from, char *to){
+    while ( *from){
+        *to++ = *from++;
+    }
+    *to = '\0';
 }
